3162843009f23fda373e9df7426413183c7d314b
Karl <karljoyeux@gmail.com>
2025-03-05 01:49:53 +0100

    creating my tests for the robot cards

--- component/UI/RobotCard/robot-card.test.tsx
+++ component/UI/RobotCard/robot-card.test.tsx
@@ -2,10 +2,22 @@ import { render } from '@testing-library/react'
 import React from 'react';
 import RobotCard from './robot-card'
 
+
+//j'aurais aimé mocker ça 
+let robotData = {
+    name: 'kalou', 
+    email: 'kalou@kalou.com'
+}
+
 it('shall be a RobotCard', () => {
-    let component = render(<RobotCard name='Kalou' email='kalou@gmail.com' />); 
+    let component = render(<RobotCard name={robotData.name} email={robotData.email} />); 
     //on veut qu'il y ait les bons éléments 
     expect(component.getByRole('img')).toBeDefined()
     expect(component.getByRole('paragraph')).toBeDefined()
     expect(component.getByRole('heading')).toBeDefined()
+})
+
+it('shall stay like a RoboCard', () => {
+    let component = render(<RobotCard name={robotData.name} email={robotData.email}/>)
+    expect(component).toMatchSnapshot()
 })
--- component/UI/RobotCard/robot-card.tsx
+++ component/UI/RobotCard/robot-card.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-
+import {v7 as uuidv7} from 'uuid'
 type RobotCardArgs = {
     email: string, name: string
 }
 
-const RobotCard = ({name, email}: RobotCardArgs) => {
-    let id = 5;
+const RobotCard = ({ name, email }: RobotCardArgs) => {
+    let Id = uuidv7()
     return (<>
-        <div>
-            <img src={`https://robohash.org/${id}?size=200x200`} />
-            <span>
-                <h2>{name}</h2>
-                <p>{ email } </p>
+        <div id={Id} className="flex w-3xs flex-col justify-center items-center rounded-2xl bg-teal-200">
+            <img src={`https://robohash.org/${Id}?size=200x200`} className="my-4"/>
+            <span className="grow flex flex-col justify-center items-center my-4">
+                <h2 className="w-fit text-xl font-semibold font-[system-ui] text-black">{name}</h2>
+                <p className="font-[system-ui] text-black font-medium">{ email } </p>
             </span>
     </div>
     </>);
