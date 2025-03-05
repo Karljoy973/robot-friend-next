caa0d7dadfa58b730cd95be96414061d98c2609b
Karl <karljoyeux@gmail.com>
2025-03-05 02:08:15 +0100

    solving dynamic ids issues in snapshot tests

--- component/UI/RobotCard/robot-card.test.tsx
+++ component/UI/RobotCard/robot-card.test.tsx
@@ -1,21 +1,27 @@
 import { render } from '@testing-library/react'
 import React from 'react';
-import RobotCard from './robot-card'
+import RobotCard, { RobotCardArgs } from "./robot-card";
 
+//j'aurais aimé mocker ça
+let robotData: RobotCardArgs = {
+	name: "kalou",
+	email: "kalou@kalou.com",
+	robotId: "rkbkjkqfdqk",
+};
 
-//j'aurais aimé mocker ça 
-let robotData = {
-    name: 'kalou', 
-    email: 'kalou@kalou.com'
-}
-
-it('shall be a RobotCard', () => {
-    let component = render(<RobotCard name={robotData.name} email={robotData.email} />); 
-    //on veut qu'il y ait les bons éléments 
-    expect(component.getByRole('img')).toBeDefined()
-    expect(component.getByRole('paragraph')).toBeDefined()
-    expect(component.getByRole('heading')).toBeDefined()
-})
+it("shall be a RobotCard", () => {
+	let component = render(
+		<RobotCard
+			name={robotData.name}
+			email={robotData.email}
+			robotId={robotData.robotId}
+		/>
+	);
+	//on veut qu'il y ait les bons éléments
+	expect(component.getByRole("img")).toBeDefined();
+	expect(component.getByRole("paragraph")).toBeDefined();
+	expect(component.getByRole("heading")).toBeDefined();
+});
 
 it('shall stay like a RoboCard', () => {
     let component = render(<RobotCard name={robotData.name} email={robotData.email}/>)
--- component/UI/RobotCard/robot-card.tsx
+++ component/UI/RobotCard/robot-card.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {v7 as uuidv7} from 'uuid'
-type RobotCardArgs = {
-    email: string, name: string
+export type RobotCardArgs = {
+    email: string, name: string, robotId? : string
 }
 
-const RobotCard = ({ name, email }: RobotCardArgs) => {
+const RobotCard = ({ name, email, robotId }: RobotCardArgs) => {
     let Id = uuidv7()
     return (<>
-        <div id={Id} className="flex w-3xs flex-col justify-center items-center rounded-2xl bg-teal-200">
+        <div id={robotId?? Id} className="flex w-3xs flex-col justify-center items-center rounded-2xl bg-teal-200">
             <img src={`https://robohash.org/${Id}?size=200x200`} className="my-4"/>
             <span className="grow flex flex-col justify-center items-center my-4">
                 <h2 className="w-fit text-xl font-semibold font-[system-ui] text-black">{name}</h2>
