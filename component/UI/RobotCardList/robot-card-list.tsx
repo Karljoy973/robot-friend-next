92ad3b97325061765e3ef90b315289bfc65d9f47
Karl <karljoyeux@gmail.com>
2025-03-05 04:00:51 +0100

    creating the grid display for robot card list

--- component/UI/RobotCardList/robot-card-list.tsx
+++ component/UI/RobotCardList/robot-card-list.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { RobotCardArgs } from "../RobotCard/robot-card";
+import RobotCard, { RobotCardArgs } from "../RobotCard/robot-card";
 
-type RobotCardListArgs = {data: RobotCardArgs[]}
-const RobotCardList = ({data}:RobotCardListArgs) => {
-    return (
-        <>
-            RobotCardList
-        </>
-    );
-}
+type RobotCardListArgs = { data: RobotCardArgs[] };
+const RobotCardList = ({ data }: RobotCardListArgs) => {
+	let items = data.map((e) => (
+		<li className="mx-4 my-2" key={e.name + e.email + Date.now()}>
+			<RobotCard name={e.name} email={e.email} />
+		</li>
+	));
+	return (
+		<div>
+			<ul className="flex flex-wrap">{items}</ul>
+		</div>
+	);
+};
  
 export default RobotCardList;
