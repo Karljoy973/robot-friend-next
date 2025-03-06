6dc84d238fd45fc9a48873f800e4352de057e1a1
Karl <karljoyeux@gmail.com>
2025-03-06 14:50:23 +0100

    improving the project configuration, I can now test and dev at the same time

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import type { NextConfig } from "next";
+import phase from 'next/constants'
+import {
+  unstable_getResponseFromNextConfig,
+} from 'next/experimental/testing/server'
 
 const nextConfig: NextConfig = {
   compiler: {
     reactRemoveProperties: true,
   },
+  async redirects() {
+    return [{ source: '/test', destination: '/test2', permanent: false }]
+  },
+  
   /* config options here */
 };
 
-export default nextConfig;
+const response = await unstable_getResponseFromNextConfig({
+  url: "https://nextjs.org/test", 
+  nextConfig
+})
+
+export default response;
