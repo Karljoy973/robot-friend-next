0586495b2caca3a5b0b03d24fa10ff33aa004672
Karl <karljoyeux@gmail.com>
2025-03-11 16:13:13 +0100

    committing to show my issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import React from "react";
+import * as React from "react";
 import { Audiowide } from "next/font/google";
 import "./globals.css";
 
--- component/atoms/RobotFriendsTypography/the-robot-friends-header.tsx
+++ component/atoms/RobotFriendsTypography/the-robot-friends-header.tsx
@@ -1,29 +1,33 @@
 const TheRobotFriendsHeader = () => {
     return (
-        <>
-   <div className="relative inline-block min-h-screen content-center justify-items-center">
-      <div className="relative">
-                    <h1 className="text-black 
+		<>
+			<div className="robot-friends-header relative inline-block min-h-screen content-center justify-items-center">
+				<div className="relative">
+					<h1
+						data-testid="back-title"
+						className="text-black 
                                     text-[128px]
-                                    font-bold tracking-normal
-                                    back-text">
-          Robot Friends
-        </h1>
-                    <h1 className="absolute 
+                                    font-bold 
+                                    tracking-normal
+                                    ">
+						Robot Friends
+					</h1>
+					<h1
+						data-testid="front-title"
+						className="absolute 
                                     top-[15px]
                                     left-[1px]
                                     text-[115px]
                                     font-bold
                                     tracking-[7px]
                                     text-white
-                                    front-text ">
-          Robot Friends
-        </h1>
-      </div>
-    </div>
-    </>
-    
-);
+                                     ">
+						Robot Friends
+					</h1>
+				</div>
+			</div>
+		</>
+	);
 }
  
 export default TheRobotFriendsHeader;
