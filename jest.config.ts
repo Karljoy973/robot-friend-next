1352ebb400057a1fd5a70cda0817f27ca9187be7
Karl <karljoyeux@gmail.com>
2025-03-12 18:08:54 +0100

    test setup and path alias are working great

--- jest.config.ts
+++ jest.config.ts
@@ -92,9 +92,9 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '@/(.*)$': "<rootDir>/$1",
-    '@/app/(.*)$': '<rootDir>/app/$1',
-    '@/types/(.*)$':'<rootDir>/types/$1',
-    '^@/components/(.*)$': '<rootDir>/components/$1',
+    'app/(.*)$': '<rootDir>/app/$1',
+    '_types/(.*)$':'<rootDir>/types/$1',
+    '^component/(.*)$': '<rootDir>/component/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
